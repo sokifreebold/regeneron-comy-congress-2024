59d06b9735c5ab967ae790823ea4467eec59299e
Sahil David <sahil.david@boldsci.com>
2023-11-16 16:55:05 +0000

    Updating nuxt to vue

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	nitro: {
+		serveStatic: true,
+	},
 	devtools: { enabled: true },
 	app: {
 		head: {
 			title: 'AHS Microsite - Regeneron',
 			link: [{ rel: 'icon', type: 'image/jpeg', href: '/favicon.jpeg' }],
 		},
+	},
+	vite: {
+		base: './',
 	},
 });
