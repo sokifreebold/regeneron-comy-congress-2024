70e888e110b27f03c46c71477c1ebb475104086d
Sahil David <sahil.david@boldsci.com>
2023-11-17 02:55:45 +0000

    debug: component include inside setup

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,3 +2,14 @@
 	<the-header />
 	<RouterView />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/TheHeader.vue';
+
+export default defineComponent({
+	components: {
+		TheHeader,
+	},
+});
+</script>
