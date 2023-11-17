749e911453fd809c58aff8e343894e95cbf3568e
Sahil David <sahil.david@boldsci.com>
2023-11-17 13:02:07 +0000

    feat: download package functionality

--- post.distribution.js
+++ post.distribution.js
@@ -3,6 +3,7 @@
  */
 const fs = require('fs-extra');
 const path = require('path');
+const archiver = require('archiver');
 
 console.log('[Distribution]: Starting the distribution process');
 
@@ -34,3 +35,60 @@ try {
 }
 
 console.log('[Distribution]: Process completed.');
+
+// Zip
+const outputPackage = path.join(__dirname, 'www/package');
+const outputZip = path.join(outputPackage, 'ash-microsite-build-package.zip');
+
+// Ensure output directory exists
+if (!fs.existsSync(outputPackage)) {
+	fs.mkdirSync(outputPackage);
+}
+
+// Create a file to stream archive data to
+const output = fs.createWriteStream(outputZip);
+const archive = archiver('zip', {
+	zlib: { level: 9 }, // Sets the compression level
+});
+
+output.on('close', function () {
+	console.log(`Archive created. Total bytes: ${archive.pointer()}`);
+});
+
+archive.on('error', function (err) {
+	throw err;
+});
+
+// Pipe archive data to the file
+archive.pipe(output);
+
+// Function to recursively walk through a directory
+function walkDir(dir) {
+	let results = [];
+	const list = fs.readdirSync(dir);
+
+	list.forEach(function (file) {
+		file = path.resolve(dir, file);
+		const stat = fs.statSync(file);
+		if (stat && stat.isDirectory()) {
+			// Recurse into a subdirectory
+			results = results.concat(walkDir(file));
+		} else {
+			// Is a file
+			results.push(file);
+		}
+	});
+	return results;
+}
+
+// Append files from source directory
+const files = walkDir(inputPath);
+
+files.forEach((file) => {
+	const filePath = file;
+	const relativePathInZip = path.relative(inputPath, file); // To preserve folder structure in the zip
+	archive.file(filePath, { name: relativePathInZip });
+});
+
+// Finalize the archive
+archive.finalize();
