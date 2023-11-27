89554bf77d9974e8c61b959d72fc9ef6bd018665
Sahil David <sahil.david@boldsci.com>
2023-11-27 13:48:05 +0000

    Google analytics plugin added

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueGtag from 'vue-gtag';
 
 import App from './App.vue';
 import router from './router';
@@ -12,6 +13,14 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
+
+app.use(
+	VueGtag,
+	{
+		config: { id: 'G-DB0JMEGR0Q' },
+	},
+	router,
+);
 
 // Components
 app.mount('#app');
