56fdaebff608175e4ee85ba9156686b77312e5e3
Sahil David <sahil.david@boldsci.com>
2024-02-28 19:46:15 +0000

    DSE Tracking

---
+++ dse/analytics.js
@@ -0,0 +1,43 @@
+function addClickListener(e) {
+	let value = '';
+	let pageId = '';
+	let pagePath = '';
+
+	const { target } = e;
+	if (target) {
+		const text = target.innerText;
+		if (text) {
+			value = text;
+		}
+	}
+
+	try {
+		const slideIndex = document.getElementById('coreSpr_2951090').getCore().B.qa();
+		if (!isNaN(slideIndex)) {
+			pageId = slideIndex + 1;
+			pagePath = `slide${pageId}`;
+		}
+	} catch (err) {
+		console.warn(err);
+	}
+
+	const obj = {
+		event: 'click',
+		pageId,
+		pagePath,
+		value,
+	};
+	console.log(`[TRACKING]: ${JSON.stringify(obj)}`);
+
+	// Offline tracking
+	if (window.electronAPI && typeof window.electronAPI.appendToCSV === 'function') {
+		window.electronAPI.appendToCSV(obj);
+	}
+}
+
+(function () {
+	document.addEventListener('DOMContentLoaded', (event) => {
+		console.log('DOM fully loaded and parsed');
+		document.addEventListener('mousedown', addClickListener);
+	});
+})();
