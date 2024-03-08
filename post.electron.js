054bee7951109f32cc77e9af5e2757070abe6e5c
Sahil David <sahil.david@boldsci.com>
2024-03-08 11:53:00 +0000

    feat: electron package build process

---
+++ post.electron.js
@@ -0,0 +1,6 @@
+// TODO: Write the code to zip the .exe file from electron/distribution to www/package folder (aacr-panels-v1.zip)
+// Once added, the files will be available to download at https://aacr-congress.dev.tech.colabglo.com/package/aacr-panels-v1.zip
+
+// Similar code is in post.distribution.js for archiving microsite
+
+console.log('Electron zip package in progress (developer working)');
