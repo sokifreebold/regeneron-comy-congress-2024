c5474d43b6e76759c0a17c283d7c6aa3aa597290
Sofia <sofia.kilani-freeman@boldsci.com>
2024-03-19 08:38:31 +0000

    chore: adding data for nsclc and melanoma

--- app/src/data/kiosk/melanoma.ts
+++ app/src/data/kiosk/melanoma.ts
@@ -12,6 +12,10 @@ const datum: ITrialsRecords[] = [
 				nct: 'NCT05352672',
 				recruiting: true,
 				trialCardPdf: 'NCT05352672_FIA-EM-0003',
+				trialCardImages: [
+					'NCT05352672_FIA-EM-0003_353061533400_844REGN-MID_L1d_front',
+					'NCT05352672_FIA-EM-0003_353061533400_844REGN-MID_L1d_back',
+				],
 				labels: {
 					title: 'Fianlimab <span class="ui-one-word">(LAG-3 antibody)</span><br>+ Cemiplimab <span class="ui-one-word">(PD-1 antibody)</span>',
 					aside: 'Previously Untreated Unresectable Locally Advanced or Metastatic Melanoma',
@@ -24,6 +28,10 @@ const datum: ITrialsRecords[] = [
 				nct: 'NCT05608291',
 				recruiting: true,
 				trialCardPdf: 'NCT05608291_FIA-EM-0009',
+				trialCardImages: [
+					'NCT05608291_FIA-EM-0009_353061533400_844REGN-MID_L1b_front',
+					'NCT05608291_FIA-EM-0009_353061533400_844REGN-MID_L1b_back',
+				],
 				labels: {
 					title: 'Fianlimab <span class="ui-one-word">(LAG-3 antibody)</span><br>+ Cemiplimab <span class="ui-one-word">(PD-1 antibody)</span>',
 					aside: 'Adjuvant use in completely resected high-risk melanoma',
@@ -37,6 +45,7 @@ const datum: ITrialsRecords[] = [
 				recruiting: true,
 				externalLink:
 					'https://clinicaltrials.gov/study/NCT06190951?term=NCT06190951&rank=1',
+				qrCode: 'QR_NCT06190951',
 				labels: {
 					title: 'Fianlimab <span class="ui-one-word">(LAG-3 antibody)</span><br>+ Cemiplimab <span class="ui-one-word">(PD-1 antibody)</span>',
 					aside: 'Resectable stage III and IV melanoma',
